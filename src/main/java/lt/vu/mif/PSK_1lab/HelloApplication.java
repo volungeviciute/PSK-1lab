a9b7e4d79bd44724e2c13c24c829bf3330ed7d2a
volungeviciute <laima.volungeviciute@gmail.com>
2021-03-13 23:17:54 +0200

    Init Java EE 8 web profile

---
+++ src/main/java/lt/vu/mif/PSK_1lab/HelloApplication.java
@@ -0,0 +1,9 @@
+package lt.vu.mif.PSK_1lab;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/lt/vu/mif/PSK_1lab/HelloResource.java
@@ -0,0 +1,14 @@
+package lt.vu.mif.PSK_1lab;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
