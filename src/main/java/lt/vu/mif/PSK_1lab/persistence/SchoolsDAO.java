26cd18d9f43593eb3381732e089416144fae875d
volungeviciute <laima.volungeviciute@gmail.com>
2021-03-30 17:30:15 +0300

    added DAO and index.xhtml

---
+++ src/main/java/lt/vu/mif/PSK_1lab/persistence/SchoolsDAO.java
@@ -0,0 +1,24 @@
+package lt.vu.mif.PSK_1lab.persistence;
+
+import lt.vu.mif.PSK_1lab.Entities.School;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@ApplicationScoped
+public class SchoolsDAO {
+    @Inject
+   private EntityManager em;
+
+    public List<School> loadAll() { return null;}
+
+    public void persist(School school){
+        this.em.persist(school);
+    }
+
+    public School findOne(Integer id){ return null;}
+
+    public void setEm(EntityManager em){this.em = em;}
+}
---
+++ src/main/java/lt/vu/mif/PSK_1lab/usecases/Schools.java
@@ -0,0 +1,37 @@
+package lt.vu.mif.PSK_1lab.usecases;
+
+import java.util.List;
+import lombok.Setter;
+import lombok.Getter;
+
+import lt.vu.mif.PSK_1lab.Entities.School;
+import lt.vu.mif.PSK_1lab.persistence.SchoolsDAO;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@Model
+public class Schools {
+    @Inject
+    private SchoolsDAO schoolsDAO;
+
+    @Getter @Setter
+    private School schoolToCreate = new School();
+
+    @Getter
+    private List<School> allSchools;
+
+    @PostConstruct
+    public void init(){loadAllSchools();}
+
+    @Transactional
+    public String createSchool(){
+        this.schoolsDAO.persist(schoolToCreate);
+        return "index?faces-redirect=true";
+    }
+
+    private void loadAllSchools(){//this.allSchools = schoolsDAO.loadAll();
+    }
+}
