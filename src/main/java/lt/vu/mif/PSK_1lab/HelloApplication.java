a2f9307ba111900bfeb179904ea473c9032a04ea
volungeviciute <laima.volungeviciute@gmail.com>
2021-04-26 22:14:26 +0300

    removed starter classes

--- src/main/java/lt/vu/mif/PSK_1lab/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package lt.vu.mif.PSK_1lab;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
