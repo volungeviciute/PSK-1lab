83bfa6738a6d4d24ff3ad3565b74324d5254f03a
volungeviciute <laima.volungeviciute@gmail.com>
2021-05-03 18:06:23 +0300

    added decorator class

--- src/main/java/lt/vu/mif/PSK_1lab/Decorator/NameChangerDecorator.java
+++ src/main/java/lt/vu/mif/PSK_1lab/Decorator/NameChangerDecorator.java
@@ -1,2 +1,27 @@
-package lt.vu.mif.PSK_1lab.Decorator;public class NameChangerDecorator {
+package lt.vu.mif.PSK_1lab.Decorator;
+
+import lt.vu.mif.PSK_1lab.Alternative.INameChanger;
+import lt.vu.mif.PSK_1lab.Entities.School;
+
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import java.util.Locale;
+
+@Decorator
+public abstract class NameChangerDecorator implements INameChanger {
+    @Inject @Delegate @Any
+    private INameChanger nameChanger;
+
+    public void ChangeName(School school) {
+        nameChanger.ChangeName(school);
+        school.setName(school.getName().toUpperCase(Locale.ROOT));
+    }
+
+    public void ChangeName(lt.vu.mif.PSK_1lab.mybatis.model.School school) {
+        nameChanger.ChangeName(school);
+        school.setName(school.getName().toUpperCase(Locale.ROOT));
+    }
+
 }
